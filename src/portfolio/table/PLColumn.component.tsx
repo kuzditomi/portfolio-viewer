7f559452dfaf2750e38048d61e44fd60335a1d7f
Küzdi Tamás <kuzditomi@gmail.com>
2019-11-18 22:00:13 +0100

    style pl column

--- src/portfolio/table/PLColumn.component.tsx
+++ src/portfolio/table/PLColumn.component.tsx
@@ -24,7 +24,8 @@ export interface PLColumnProps {
 }
 
 const getPLString = (pl: number) => {
-  return pl < 0 ? (pl * -1).toFixed(2) : pl.toFixed(2);
+  const amount = pl * 100;
+  return amount < 0 ? (amount * -1).toFixed(2) : amount.toFixed(2);
 };
 
 const PLColumn: React.FC<PLColumnProps & WithStyles<typeof styles>> = ({
@@ -32,8 +33,11 @@ const PLColumn: React.FC<PLColumnProps & WithStyles<typeof styles>> = ({
   classes
 }) => {
   return (
-    <TableCell align="right" className={pl > 0 ? classes.win : pl < 0 ? classes.loss : ''}>
-      {getPLString(pl)}
+    <TableCell
+      align="right"
+      className={pl > 0 ? classes.win : pl < 0 ? classes.loss : ""}
+    >
+      {getPLString(pl)} $
     </TableCell>
   );
 };
